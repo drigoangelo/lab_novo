94ef90521165ba160e4c0b7ff0bded0f7536ce25
Cauêh Queiroz <caueh@equilibriumweb.com>
2019-04-10 09:29:35 -0300

    Ajuste do nível de reporte de erros

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ini_set("display_errors",1);
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 define("IGENIAL_ROOT_DIR", dirname(__FILE__));
 include(dirname(__FILE__)."/framework/controller/FrontController.php");
 #Util::debug($_REQUEST);
