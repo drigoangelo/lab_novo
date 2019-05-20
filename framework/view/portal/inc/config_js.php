9cb0f6cfb44534881ee55e5a90ff0687f9b990b9
Cauêh Queiroz <caueh@equilibriumweb.com>
2019-05-20 14:17:49 -0300

    Ajuste para IP público

--- framework/view/portal/inc/config_js.php
+++ framework/view/portal/inc/config_js.php
@@ -31,7 +31,7 @@
 
 <?php if($oAluno = AlunoAction::recuperaObjetoAluno() !== false) { ?>
   <script type="text/javascript">
-    var host = 'http://10.1.68.251:3000';
+    var host = 'http://200.19.144.7:3000';
 
       (function(w, d, s, u) {
 
