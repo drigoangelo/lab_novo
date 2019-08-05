56a7cecda8b0a47ea5b63dab767815064de57214
fabianos <fsralves@ufu.br>
2019-08-05 17:37:27 -0300

    Criação método makeFileUploadRoot

--- framework/bib/util/file/FileUtil.php
+++ framework/bib/util/file/FileUtil.php
@@ -26,4 +26,29 @@ class FileUtil extends FileUtilParent {
         }
     }
 
+    public static function makeFileUploadRoot($entityPath, $id, $field, $edit) {
+        if (!empty($_FILES[$field]) ) {
+            $filename = basename($_FILES[$field]['name']);
+
+            $ext = strtolower(substr($filename, strrpos($filename, '.') + 1));
+            $newname = LOCAL_CAMERA . "/$entityPath/$id.$ext";
+
+            if ($edit) {
+                self::removeAllFiles(LOCAL_CAMERA . "/$entityPath/", $id);
+                self::removeAllFiles(LOCAL_CAMERA . "/$entityPath/", "{$id}_c");
+                self::removeAllFiles(LOCAL_CAMERA . "/$entityPath/", "{$id}_t");
+            }
+            if (!file_exists(dirname($newname))) {
+                mkdir(dirname($newname), 0777, true);
+            }
+            chmod($_FILES[$field]['tmp_name'], 0777);
+            if (!(copy($_FILES[$field]['tmp_name'], $newname))) {
+                throw new Exception("Atenção: Ocorreu um problema ao copiar o arquivo para a pasta de upload!");
+            }
+            chmod($newname, 0777);
+            
+            return "$id.$ext";
+        }
+    } 
+
 }
