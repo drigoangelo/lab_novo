30c912f20584c22d0cb4a29729cf279b3fa5ea62
Rodrigo Angelo <rangelo@synvata.com>
2020-05-08 12:46:37 -0300

    Incluindo arquivos que estavam no servidor de produção mas não estavam no git.

--- src/index.php
+++ src/index.php
@@ -1,4 +1,5 @@
 <?php
+ini_set("display_errors",1);
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 define("IGENIAL_ROOT_DIR", dirname(__FILE__));
 include(dirname(__FILE__)."/framework/controller/FrontController.php");
